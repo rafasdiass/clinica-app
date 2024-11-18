18bd2c1d7725264deb2b3c82beabc17f5958e332
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2024-11-18 19:25:32 -0300

    feat: card de agendamento

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 import { IonHeader, IonContent } from "@ionic/angular/standalone";
 import { IonicModule } from '@ionic/angular';
 import { FooterPage } from '../footer/footer.page';
+import { AppointmentsPage } from "../pages/appointments/appointments.page";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, HeaderComponent, IonicModule, FooterPage], // Importa o HeaderComponent standalone
+  imports: [ HeaderComponent, IonicModule, FooterPage, AppointmentsPage], // Importa o HeaderComponent standalone
 })
 export class HomePage {}
